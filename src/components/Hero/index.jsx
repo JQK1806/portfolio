4554515d60c05d6cb8fdb3f307e02fe8d324ab94
JQK1806 <king.jos@northeastern.edu>
2025-07-23 23:47:01 -0400

    Remove image and center text
    Remove the temp image and center the text in the hero section

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import './index.css'
-import logo from '../../assets/react.svg'
 
 const Hero = () => {
     return (
@@ -9,8 +8,6 @@ const Hero = () => {
                 <h1>Hello I'm <span>Joseph King</span></h1>
                 <h2>Placeholder...</h2>
             </div>
-            {/* TEMP IMG */}
-            <img src={logo} alt="Joseph King - Developer" />
         </div>
     )
 }
