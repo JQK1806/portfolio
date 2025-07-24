f835e46ac27c6898d52700fe1568ef44104cc7ff
Joseph King <150943055+JQK1806@users.noreply.github.com>
2025-07-23 23:52:21 -0400

    Update hero (#19)
    * Remove image and center text
    
    Remove the temp image and center the text in the hero section
    
    * add job titles to hero

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -1,16 +1,13 @@
 import React from "react";
 import './index.css'
-import logo from '../../assets/react.svg'
 
 const Hero = () => {
     return (
         <div className="hero">
             <div className="hero-about">
                 <h1>Hello I'm <span>Joseph King</span></h1>
-                <h2>Placeholder...</h2>
+                <h2>Software Engineer | Data Engineer | Data Analyst</h2>
             </div>
-            {/* TEMP IMG */}
-            <img src={logo} alt="Joseph King - Developer" />
         </div>
     )
 }
